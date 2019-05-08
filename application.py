cc6de8431d230ebc1e918dc61b510e1130ef7804
TylerGlisson <tyler@tglisson.com>
2019-05-08 17:53:32 -0400

    added port number to application.py

--- application.py
+++ application.py
@@ -100,3 +100,7 @@ def update():
 
     # output places as JSON
     return jsonify(rows)
+
+if __name__ == "__main__":
+ port = int(os.environ.get("PORT", 8080))
+ app.run(host="0.0.0.0", port=port)
